8aac5455c5e0632fe6696b350817e6610e1de72c
solfire1994 <solfire1994@gmail.com>
2023-04-17 20:33:33 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
+using WPFBankDepartmentMVVM.ViewModels;
+
+namespace WPFBankDepartmentMVVM
+{
+    public partial class App : Application
+    {
+        private static IServiceProvider? _Services;
+
+        public static IServiceProvider Services => _Services ??= InitializeServices().BuildServiceProvider();
+
+        private static IServiceCollection InitializeServices()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<MainWindowViewModel>();
+            services.AddTransient<AuthWindowViewModel>();
+            services.AddTransient<AddNewClientViewModel>();
+            services.AddTransient<ChangingLogViewModel>();
+            services.AddTransient<ClientWindowViewModel>();
+            services.AddTransient<TransferWindowViewModel>();
+
+            return services;
+        }
+    }
+
+
+}
---
+++ Command/Base/BaseCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPFBankDepartmentMVVM.Command.Base
+{
+    internal class BaseCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public BaseCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            canExecute = CanExecute;
+        }
+
+        public bool CanExecute(object parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => execute(parameter);
+    }
+}
